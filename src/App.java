860b1e220c52b6053fd8e4a96bc6e6930436d68d
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-12-07 09:15:24 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /**
+         * In an alien language, surprisingly, they also use English lowercase
+         * letters, but possibly in a different 'order'. The order of the
+         * alphabets is some permutation of lowercase letters.
+         * 
+         * Gievn a seguence of words written in an alien language and the order
+         * of thr alphabets, return true if and only if the given words are sorted
+         * lexiographically in this alien language.
+         * 
+         * Input: words=['hello', 'leetcode'], order="hlabcdefgijkmnopqrstuvwxyz"
+         * Output: true
+         * Explanation: As 'h' comes before 'l' in this language, then the sequence is
+         * sorted.
+         * 
+         * Input: words=['word','world','row'], order='worldabcefghijkmnpqstuvxyz'
+         * Output: false
+         * Explanation: As 'd' comes after 'l' in this language, then words[0]>word[1],
+         * hence the sequence is unsorted.
+         */
+        System.out.println("Hello, World!");
+    }
+}
